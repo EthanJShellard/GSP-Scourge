b27e8370fd7d97f589c64a5b607c44f4760c9a94
Ethan Shellard <ethanshellard@gmail.com>
2021-02-03 14:37:29 +0000

    Implemented Basic Camera Follow Script

---
+++ Scourge-GSP/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Camera follow script to be placed on the main camera object. Give it the player transform in the editor and set its size for zoom (Increase size to zoom out).
+/// </summary>
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float size;
+    public float zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = playerTransform.position;
+        transform.Translate(0,0, zPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position;
+        transform.Translate(0, 0, zPos);
+    }
+}
