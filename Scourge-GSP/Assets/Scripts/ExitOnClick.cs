c775da2cbe027fe48cc3b95b62c624f12412605e
sadiesundt <78212867+sadiesundt@users.noreply.github.com>
2021-02-08 13:53:15 +0000

    Menu Implementation
    Hopefully this menu will work, this has the simple controls for a menu and exit.

---
+++ Scourge-GSP/Assets/Scripts/ExitOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SUMMARY
+// Quits the game when clicked
+// END OF SUMMARY
+
+public class ExitOnClick : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Scourge-GSP/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//SUMMARY
+//This loads the scene for the game
+//END OF SUMMARY
+
+public class LoadOnClick : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
