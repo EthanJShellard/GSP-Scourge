5ea6a90584e9e0716a29e9e3410cfa5744ab183c
Andrew Neate <43048523+andynrocks@users.noreply.github.com>
2021-02-08 12:25:15 +0000

    basic movement
    Left and right movement done

--- Scourge-GSP/Assets/Scripts/PlayerController.cs
+++ Scourge-GSP/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public float speed;
+    [Range(0, .3f)] [SerializeField] private float moveSmoothing = .05f;
 
+    private Rigidbody2D rb;
+    private float horizontalInput;
+    private Vector3 baseVelocity = Vector3.zero;
+    private bool facingRight = true;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
     }
+
+    void FixedUpdate()
+    {
+        Vector3 targetVelocity = new Vector2(horizontalInput * speed * Time.fixedDeltaTime * 10, rb.velocity.y);
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref baseVelocity, moveSmoothing);
+
+        if (horizontalInput > 0 && !facingRight)
+        {
+            FlipPlayer();
+        }
+        else if (horizontalInput < 0 && facingRight)
+        {
+            FlipPlayer();
+        }
+    }
+
+    private void FlipPlayer()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
 }
