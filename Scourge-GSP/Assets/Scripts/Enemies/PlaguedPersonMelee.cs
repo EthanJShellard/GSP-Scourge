0deb0710918a35697443836214c8d553c4032fd8
Ethan Shellard <ethanshellard@gmail.com>
2021-02-09 13:36:51 +0000

    Some Prefab changes and more enemy foundations

---
+++ Scourge-GSP/Assets/Scripts/Enemies/PlaguedPersonMelee.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaguedPersonMelee : Enemy
+{
+    [SerializeField] private int attackDamage;
+    [SerializeField] private int walkSpeed;
+    [SerializeField] private int aggroRange;
+    [SerializeField] private LayerMask playerLayer;
+
+    private void FixedUpdate()
+    {
+        Collider2D[] player = Physics2D.OverlapCircleAll(transform.position, aggroRange, playerLayer);
+        if (player.Length > 0) 
+        {
+            FindPathToPlayer(player[0].transform);
+        }
+    }
+
+    private void FindPathToPlayer(Transform player) 
+    {
+        
+    }
+
+    override public void Kill() 
+    {
+        /*
+            TODO:
+            Blood Effect
+            Corpse?? Delete Enemy?
+         */
+    }
+
+    public override void Damage(int n)
+    {
+        hitPoints -= n;
+
+        if (hitPoints <= 0) 
+        {
+            Kill();
+        }
+    }
+
+    //Default enemy values
+    private void Reset()
+    {
+        hitPoints = 2;
+        attackDamage = 1;
+        walkSpeed = 1;
+        aggroRange = 10;
+    }
+
+}
