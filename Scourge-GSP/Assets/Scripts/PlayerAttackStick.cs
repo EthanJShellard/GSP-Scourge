9342843df6b2387599430291629b7ad5e6da01ed
sadiesundt <78212867+sadiesundt@users.noreply.github.com>
2021-02-15 14:33:54 +0000

    Stick Attack Script
    Attempted to code the script attack, but i have blocked out the basic things that it should have. Not been tested yet

---
+++ Scourge-GSP/Assets/Scripts/PlayerAttackStick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackStick : MonoBehaviour
+{
+    private float timeAttack;
+    public float startTimeAttack;
+
+    public Transform attackPos;
+    public LayerMask enemies;
+    public float attackRange;
+    public int damage;
+
+    void Update()
+    {
+        if(timeAttack <= 0)
+        {
+            if(Input.GetKey(KeyCode.Mouse0))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().health -= damage;
+                }
+            }
+            timeAttack = startTimeAttack;
+        }
+        else
+        {
+            timeAttack -= Time.deltaTime;
+        }
+    }
+}
