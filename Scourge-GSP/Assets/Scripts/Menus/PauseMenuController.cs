ed8447d06204c6db60b588a5335d87666bb4efef
Andrew Neate <43048523+andynrocks@users.noreply.github.com>
2021-03-19 15:38:52 +0000

    Pause menu now implemented
    press escape and find out

---
+++ Scourge-GSP/Assets/Scripts/Menus/PauseMenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    bool isOpen = false;
+
+    void Start()
+    {
+
+        pauseMenu.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isOpen = !isOpen;
+        }
+
+        if (isOpen)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void OnClick(Button btn)
+    {
+        switch (btn.tag)
+        {
+            case "Resume":
+                isOpen = false;
+                break;
+            case "MainMenu":
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1;
+                SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+        }
+    }
+
+}
