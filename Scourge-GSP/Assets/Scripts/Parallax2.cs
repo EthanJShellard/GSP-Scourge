bdcb627bf9b03710700ce1f855b0eaf7548a3963
sjrogers88 <78164766+sjrogers88@users.noreply.github.com>
2021-04-02 18:03:19 +0100

    Background Functional

--- Scourge-GSP/Assets/Scripts/Parallax2.cs
+++ Scourge-GSP/Assets/Scripts/Parallax2.cs
@@ -8,36 +8,18 @@ public class Parallax2 : MonoBehaviour
 
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
-    private float textureUnitSizeX;
-    private float textureUnitSizeY;
 
-    void Start()
+    private void Start()
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        Texture2D texture = sprite.texture;
-        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
-        textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
         transform.position -= new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
         lastCameraPosition = cameraTransform.position;
-
-        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
-        {
-            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, cameraTransform.position.y);
-        }
-
-        if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
-        {
-            float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
-            transform.position = new Vector3(cameraTransform.position.y + offsetPositionY, cameraTransform.position.y);
-        }
     }
 }
  
