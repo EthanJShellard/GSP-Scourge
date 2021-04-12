b73dd825cda0aa49d17b9db266eb2b8e597551fa
sadiesundt <78212867+sadiesundt@users.noreply.github.com>
2021-04-12 22:13:28 +0100

    Added script to control tutorial popup

---
+++ Scourge-GSP/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public GameObject tutorials;
+    private float tutorialTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorials.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Tutorial")
+        {
+            tutorials.SetActive(true);
+            tutorialTimer = 100.0f;
+            tutorialTimer -= Time.deltaTime;
+
+            if(tutorialTimer <=0)
+            {
+                tutorials.SetActive(false);
+            }
+        }
+    }
+}
