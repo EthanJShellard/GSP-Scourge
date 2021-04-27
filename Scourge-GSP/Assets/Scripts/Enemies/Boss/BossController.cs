6379a8d90bfbfca5faf266fc384a36adc7312682
Ethan Shellard <ethanshellard@gmail.com>
2021-04-27 11:43:17 +0100

    Fixed melee

--- Scourge-GSP/Assets/Scripts/Enemies/Boss/BossController.cs
+++ Scourge-GSP/Assets/Scripts/Enemies/Boss/BossController.cs
@@ -198,7 +198,7 @@ public class BossController : MonoBehaviour
 
     Attack getRandomAttack()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) <= melleeRange) return Attack.Mellee;
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) <= melleeRange) return Attack.Mellee;
         
         Attack nextAttack;
         int randomNumber = Random.Range(0, 2);
