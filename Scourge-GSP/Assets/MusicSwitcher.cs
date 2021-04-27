3834f4267ce44a754e3cf04ac29dfc58d523856e
Ethan Shellard <ethanshellard@gmail.com>
2021-04-27 16:13:13 +0100

    Music implementedd

---
+++ Scourge-GSP/Assets/MusicSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour
+{
+    [SerializeField] AudioClip BossMusic;
+    private bool switched = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player" && !switched)
+        {
+            AudioSource aus = GetComponent<AudioSource>();
+            aus.Stop();
+            aus.PlayOneShot(BossMusic);
+            switched = true;
+        }
+    }
+}
